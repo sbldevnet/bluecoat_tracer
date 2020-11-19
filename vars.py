6352a772f0a4634c526d0b9352173a765b31932d
Samuel <34037255+sburgosl@users.noreply.github.com>
2020-11-19 16:35:38 +0100

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+"""
+Description:
+    Static variables file
+"""
+
+#####################
+# Proxy Variables
+#####################
+# (String) API URL
+API_URL     = 'https://example:8082/api/' # Management System
+NODE_URL    = 'https://example:8082/' # Admistration proxy 
+
+#####################
+# Policy Variables
+#####################
+# (Int) Proxy explicit port for policy trace
+proxy_port = 8080
+
+#(String)
+# AUTHENTICATION = ''
+
+# (String List)
+EXCLUDE_LAYERS = ['Test_LAYER']
+
+#####################
+# Script Variables
+#####################
+# (Boolean) Select if make requests to proxy or not
+ONLINE = True
+
+# (String) Log file name
+LOG_FILE_NAME = 'trace.log'
+
+# (String) xml file path
+FILE_PATH = '/home/bluecoat_tracer/bluecoat_policy.xml'
